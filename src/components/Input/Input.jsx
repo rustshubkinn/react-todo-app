e607c009eb49e52e429efeaded536645fbf41997
rustshubkinn <rustshubkinn@mail.ru>
2021-05-15 00:43:33 +0600

    Refactored Input Module and changed Task Class Name to Main Class Name.

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,35 +1,33 @@
-import { PropTypes } from 'prop-types';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import classes from './Input.module.scss';
 
-function Input({ onChange, type, placeholder, task, value }) {
-  return (
-    <input
-      value={value}
-      type={type}
-      placeholder={placeholder}
-      className={classNames({
-        [classes.input]: true,
-        [classes.task]: task,
-      })}
-      onChange={onChange}
-    />
-  );
-}
+const Input = ({ onChange, type, placeholder, main, value }) => (
+  <input
+    value={value}
+    type={type}
+    placeholder={placeholder}
+    onChange={onChange}
+    className={classNames({
+      [classes.input]: true,
+      [classes.main]: main,
+    })}
+  />
+);
 
 Input.propTypes = {
   onChange: PropTypes.func.isRequired,
   type: PropTypes.string,
   placeholder: PropTypes.string.isRequired,
-  task: PropTypes.bool,
+  main: PropTypes.bool,
   value: PropTypes.string,
 };
 
 Input.defaultProps = {
   value: '',
   type: 'text',
-  task: false,
+  main: false,
 };
 
 export default Input;
