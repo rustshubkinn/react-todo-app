224f1ce106d01a63f6b1a8c614082bfcea47309e
rustshubkinn <rustshubkinn@mail.ru>
2021-05-26 17:27:32 +0600

    Implemented Fetch from Firebase Database for MOCK Todos and configured Loading Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import TodoForm from './components/TodoForm/TodoForm';
 import TodoList from './components/TodoList/TodoList';
 
 import classes from './App.module.scss';
+import Loader from './components/Loader/Loader';
 
-const MOCK_TODOS = [
-  {
-    text: 'Learn React',
-    isCompleted: false,
-    id: 1,
-  },
-  {
-    text: 'Sleep',
-    isCompleted: false,
-    id: 2,
-  },
-  {
-    text: 'Build really cool todo app',
-    isCompleted: false,
-    id: 3,
-  },
-];
+const URL =
+  'https://todo-list-544c0-default-rtdb.europe-west1.firebasedatabase.app/todos.json';
 
 const App = () => {
-  const [todos, setTodos] = useState(MOCK_TODOS);
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchTodos = async () => {
+    setLoading(true);
+
+    const response = await fetch(URL);
+    const result = await response.json();
+
+    const normalizedTodos = Object.keys(result).map((k) => result[k]);
+
+    setTodos(normalizedTodos);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
 
   return (
     <div className={classes.wrapper}>
-      <div className={classes.todo_list_wrapper}>
-        <TodoForm setTodos={setTodos} />
-        <TodoList todos={todos} setTodos={setTodos} />
-      </div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className={classes.todo_list_wrapper}>
+          <TodoForm setTodos={setTodos} />
+          <TodoList todos={todos} setTodos={setTodos} />
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+import classes from './Loader.module.scss';
+
+const Loader = () => (
+  <div className={classes.ldsroller}>
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+);
+
+export default Loader;
