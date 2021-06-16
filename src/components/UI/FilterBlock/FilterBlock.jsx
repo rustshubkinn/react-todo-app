25e54fb416a01c07ecbc5df01ecc44527a19b926
rustshubkinn <rustshubkinn@mail.ru>
2021-06-16 10:11:49 +0600

    Implemented FilterBlock with Select component in App.jsx. Add styles for Select and FilterBlock  in App.Module.scss.

---
+++ src/components/UI/FilterBlock/FilterBlock.jsx
@@ -0,0 +1,42 @@
+import classNames from 'classnames';
+import { bool, element, func, string } from 'prop-types';
+import Button from '../Button/Button';
+import classes from './FilterBlock.module.scss';
+
+const FilterBlock = ({ children, onSubmit, className, block }) => {
+  const renderFilters = () => {
+    if (!children.length) {
+      return <li>{children}</li>;
+    }
+
+    return children.map((filter) => <li>{filter}</li>);
+  };
+
+  return (
+    <section
+      className={classNames({
+        [classes.filter_block]: block,
+        [className]: className,
+      })}
+    >
+      <form onSubmit={onSubmit}>
+        <ul className={classes.filter_block_list}>{renderFilters()}</ul>
+        <Button type="submit">Search</Button>
+      </form>
+    </section>
+  );
+};
+
+FilterBlock.propTypes = {
+  children: element.isRequired,
+  onSubmit: func.isRequired,
+  className: string,
+  block: bool,
+};
+
+FilterBlock.defaultProps = {
+  className: null,
+  block: false,
+};
+
+export default FilterBlock;
