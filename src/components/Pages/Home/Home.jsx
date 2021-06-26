e7c02d151d3fedff0dda5d8f914ee2a83a5a4001
rustshubkinn <rustshubkinn@mail.ru>
2021-06-27 00:08:40 +0600

    Change App Page to Home Page Route. Reconfigure index.jsx according to new route

---
+++ src/components/Pages/Home/Home.jsx
@@ -0,0 +1,96 @@
+import { useEffect, useState } from 'react';
+
+import Loader from 'components/UI/Loader/Loader';
+import TodoForm from 'components/TodoForm/TodoForm';
+import TodoList from 'components/TodoList/TodoList';
+import FilterBlock from 'components/UI/FilterBlock/FilterBlock';
+import Select from 'components/UI/Select/Select';
+
+import { fetchTodo } from 'api/api';
+
+import classes from './Home.module.scss';
+
+const Home = () => {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [value, setValue] = useState('');
+
+  const MOCK_OPTIONS = [
+    {
+      value: '',
+      name: 'All Todos',
+    },
+    {
+      value: '0',
+      name: 'Undone Todos',
+    },
+    {
+      value: '1',
+      name: 'Done Todos',
+    },
+  ];
+
+  const fetchData = async () => {
+    setLoading(true);
+    const todoList = await fetchTodo();
+    setTodos(todoList);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    if (!value) {
+      await fetchData();
+      return;
+    }
+
+    const newTodos = await fetchTodo();
+    const parsed = Boolean(parseInt(value, 10));
+
+    if (parsed === true) {
+      const filterTodo = newTodos.filter((todo) => todo.isCompleted);
+      setTodos(filterTodo);
+    } else {
+      const filterTodo = newTodos.filter((todo) => !todo.isCompleted);
+      setTodos(filterTodo);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className={classes.wrapper}>
+      <Loader loading={loading} />
+      <FilterBlock
+        block
+        className={classes.filter_block}
+        onSubmit={(e) => onSubmitHandler(e)}
+      >
+        <Select
+          name="select_filter"
+          id="select_filter"
+          options={MOCK_OPTIONS}
+          className={classes.select_filter}
+          onChange={(e) => handleChange(e)}
+          value={value}
+          filter
+        />
+      </FilterBlock>
+      <div className={classes.todo_list_wrapper}>
+        <TodoForm setTodos={setTodos} />
+        <TodoList todos={todos} setTodos={setTodos} />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import App from 'components/App/App';
-import TodoPage from 'components/Pages/TodoPage/TodoPage';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/todo/:id/:text" component={TodoPage} />
-    </Switch>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
